0025157fd8fe6153e1bef7fa3e1b1ac2aef5d38c
petrero <petrero@gmail.com>
2017-06-12 23:36:55 +0300

    So how do we create it? With the bundle it's super easy. Just go back to your fixtures class, AppFixtures and create a function called characterName. And in this function we just need to return a character name. I'll paste in a few of my favorites and then at the bottom we'll use the :phpfunction:array_rand function to return a random character each time Alice calls this.
    I love when things are this simple!
    
    app/console hautelook:fixtures:load
    Flip back to the browser and when you refresh this time, real video game heroes!

---
+++ src/AppBundle/DataFixtures/ORM/AppFixtures.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace AppBundle\DataFixtures\ORM;
+
+use Hautelook\AliceBundle\Doctrine\DataFixtures\AbstractLoader;
+use Nelmio\Alice\Fixtures;
+
+class AppFixtures extends AbstractLoader {
+  public function getFixtures() {
+    return array(__DIR__.'/fixtures.yml');
+  }
+  
+  public function characterName(){
+    $names = array(
+      'Mario',
+      'Luigi',
+      'Sonic',
+      'Pikachu',
+      'Link',
+      'Lara Croft',
+      'Trogdor',
+      'Pac-Man'  
+    );
+    
+    return $names[array_rand($names)];
+  }
+}
